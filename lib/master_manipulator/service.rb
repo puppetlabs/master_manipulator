14184e40cda5023c09b5bcbda6937f8c7b5ae78b
Zach Reichert <zach.reichert@puppetlabs.com>
2015-06-23 13:20:07 -0700

    (QA-1930) add option is_pe?

--- lib/master_manipulator/service.rb
+++ lib/master_manipulator/service.rb
@@ -7,6 +7,7 @@ module MasterManipulator
     # *+opts+ - an options hash - not required
     #   *+:timeout+ - the amount of time in seconds to wait for success
     #   *+:frequency+ - The time to wait between retries
+    #   *+:is_pe?+ - Boolean : if the SUT is PE, defaults to true
     #
     # Raises a standard error if the wait is unsuccessful
     #
@@ -15,8 +16,11 @@ module MasterManipulator
     # restart_puppet_server(master, {:time_out => 200, :frequency => 10})
     def restart_puppet_server(host, opts = {})
 
-      on(host, puppet('resource service pe-puppetserver ensure=stopped'))
-      on(host, puppet('resource service pe-puppetserver ensure=running'))
+      opts[:is_pe?] ||= true
+      opts[:is_pe?] ? service_name = 'pe-puppetserver' : service_name = 'puppetserver'
+
+      on(host, puppet("resource service #{service_name} ensure=stopped"))
+      on(host, puppet("resource service #{service_name} ensure=running"))
       masterHostName = on(host, 'hostname').stdout.chomp
       opts[:time_out] ||= 60
       opts[:frequency] ||= 5
@@ -31,7 +35,7 @@ module MasterManipulator
         case exit_code.to_s
           when '0'
             sleep 20
-            return 'Restarting the Puppet Server was successful!'
+            return
           when '1' || '7'
             # Exit code 7 is "connection refused"
             sleep opts[:frequency]
