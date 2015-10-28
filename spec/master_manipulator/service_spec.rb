ef2ebfc70f5998779324a6e36feb3c732d134194
Zach Reichert <zach.reichert@puppetlabs.com>
2015-10-28 16:49:31 -0700

    (QA-2115) use services endpoint for puppetserver

--- spec/master_manipulator/service_spec.rb
+++ spec/master_manipulator/service_spec.rb
@@ -8,6 +8,8 @@ describe MasterManipulator::Service do
   let(:cmd_1)                 { 'resource service pe-puppetserver ensure=stopped' }
   let(:cmd_2)                 { 'resource service pe-puppetserver ensure=running' }
   let(:cmd_3)                 { 'hostname' }
+  let(:successful_stdout)     { IO.read(File.expand_path('spec/files/success_body.txt')) }
+  let(:failure_stdout)        { IO.read(File.expand_path('spec/files/failure.txt')) }
 
   def shared_dsl_expectations
     expect(dummy_class).to receive(:on).with(beaker_host, beaker_command).twice
@@ -19,7 +21,7 @@ describe MasterManipulator::Service do
   describe '.restart_puppet_server' do
 
     let(:beaker_result)       { x = Beaker::Result.new('host', 'cmd')
-                                x.stdout = 'This is stdout'
+                                x.stdout = successful_stdout
                                 x.exit_code = '0'
                                 x
                               }
@@ -40,27 +42,38 @@ describe MasterManipulator::Service do
 
     context 'negative cases' do
 
-      let(:beaker_result)     { x = Beaker::Result.new('host', 'cmd')
-                                x.stdout = 'This is stdout'
-                                x.exit_code = '7'
-                                x
-                              }
+      let(:beaker_no_connection_result)     { x = Beaker::Result.new('host', 'cmd')
+                                              x.stdout = failure_stdout
+                                              x.exit_code = '7'
+                                              x
+      }
       let(:failed_curl_result)      { x = Beaker::Result.new('host', 'cmd')
-                                      x.stdout = 'This is stdout'
+                                      x.stdout = failure_stdout
                                       x.exit_code = '1'
                                       x
-                                    }
+      }
+      let(:failed_state)      { x = Beaker::Result.new('host', 'cmd')
+                                      x.stdout = failure_stdout
+                                      x.exit_code = '0'
+                                      x
+      }
 
       it 'puppet server never starts' do
         shared_dsl_expectations
-        expect(dummy_class).to receive(:curl_on).exactly(10).times.and_return(beaker_result)
-        expect{ dummy_class.restart_puppet_server(beaker_host, {:time_out => 10}) }.to raise_error(RuntimeError, "Attempted to restart 10 times, waited 5 seconds between attempts.")
+        expect(dummy_class).to receive(:curl_on).exactly(10).times.and_return(beaker_no_connection_result)
+        expect{ dummy_class.restart_puppet_server(beaker_host) }.to raise_error(RuntimeError, /Attempted to restart 10 times, waited .* seconds total/)
       end
 
       it 'should fail gracefully when curl fails' do
         shared_dsl_expectations
         expect(dummy_class).to receive(:curl_on).exactly(10).times.and_return(failed_curl_result)
-        expect{ dummy_class.restart_puppet_server(beaker_host, {:time_out => 10}) }.to raise_error(RuntimeError, "Attempted to restart 10 times, waited 5 seconds between attempts.")
+        expect{ dummy_class.restart_puppet_server(beaker_host) }.to raise_error(RuntimeError, /Attempted to restart 10 times, waited .* seconds total/)
+      end
+
+      it 'should report the service that is not running' do
+        shared_dsl_expectations
+        expect(dummy_class).to receive(:curl_on).exactly(10).times.and_return(failed_state)
+        expect{ dummy_class.restart_puppet_server(beaker_host) }.to raise_error(RuntimeError, /'pe-master' state: foo/)
       end
 
     end
